04840314983c15ff93a7b141f8f4eca0f4c8c6d3
Kraftred <48571482+KarlZellin@users.noreply.github.com>
2023-12-25 22:46:40 +0000

    Add files via upload

---
+++ themes/meutema/theme.lua
@@ -0,0 +1,201 @@
+---------------------------
+-- Default awesome theme --
+---------------------------
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local rnotification = require("ruled.notification")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+local theme = {}
+
+-- Relacionados a font
+theme.font          = "sans 8"
+
+-- Colors
+bw_0_h           = "#1d2021"
+bw_0             = "#282828"
+bw_0_s           = "#32302f"
+bw_1             = "#3c3836"
+bw_2             = "#504945"
+bw_3             = "#665c54"
+bw_4             = "#7c6f64"
+bw_5             = "#928374"
+bw_6             = "#a89984"
+bw_7             = "#bdae93"
+bw_8             = "#d5c4a1"
+bw_9             = "#ebdbb2"
+bw_10            = "#fbf1c7"
+
+black_1          = "#282828"
+black_2          = "#928374"
+red_1            = "#cc241d"
+red_2            = "#fb4934"
+green_1          = "#98971a"
+green_2          = "#b8bb26"
+yellow_1         = "#d79921"
+yellow_2         = "#fabd2f"
+blue_1           = "#458588"
+blue_2           = "#83a598"
+purple_1         = "#b16286"
+purple_2         = "#d3869b"
+aqua_1           = "#689d6a"
+aqua_2                                          = "#8ec07c"
+white_1                                         = "#a89984"
+white_2                                         = "#ebdbb2"
+orange_1                                        = "#d65d0e"
+orange_2                                        = "#fe8019"
+
+
+
+
+theme.bg_normal                                 = bw_0
+theme.bg_focus                                  = bw_2
+theme.bg_urgent                                 = bw_2
+theme.bg_minimize                               = "#444444"
+
+
+theme.fg_normal                                 = bw_9
+theme.fg_focus                                  = bw_9
+theme.fg_urgent                                 = red_2
+theme.fg_minimize                               = "#ffffff"
+
+
+theme.border_color_normal                       = bw_2  -- mesma cor da borda wibar
+theme.border_color_active                       = bw_5
+theme.border_color_marked                       = bw_5
+
+
+-- Tasklist variables
+theme.tasklist_spacing                          = 3
+theme.tasklist_disable_icon                     = true
+
+theme.tasklist_fg_normal                        = bw_5
+theme.tasklist_fg_focus                         = bw_8
+theme.tasklist_fg_minimize                      = bw_2
+theme.tasklist_fg_urgent                        = red_2
+theme.tasklist_bg_normal                        = bw_0
+theme.tasklist_bg_focus                         = bw_0
+theme.tasklist_bg_minimize                      = bw_0
+theme.tasklist_bg_urgent                        = bw_0
+
+
+
+
+theme.border                                    = 4
+theme.border_width                              = dpi(3)
+theme.useless_gap                               = dpi(5)
+theme.systray_icon_spacing                      = 4
+
+theme.bg_systray                                = bw_0
+
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+-- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+-- Variables set for theming notifications:
+-- notification_font
+-- notification_[bg|fg]
+-- notification_[width|height|margin]
+-- notification_[border_color|border_width|shape|opacity]
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(15)
+theme.menu_width  = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+
+theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+
+theme.wallpaper = themes_path.."default/background.png"
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+theme.layout_max = themes_path.."default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+theme.layout_tile = themes_path.."default/layouts/tilew.png"
+theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+-- Set different colors for urgent notifications.
+rnotification.connect_signal('request::rules', function()
+    rnotification.append_rule {
+        rule       = { urgency = 'critical' },
+        properties = { bg = '#ff0000', fg = '#ffffff' }
+    }
+end)
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
